0a7e328640f7cc2ad10c8da3bc68810c13066fec
zsolt270 <zsolt270000@gmail.com>
2024-10-10 19:20:33 +0200

    added get method

---
+++ server/router.js
@@ -0,0 +1,14 @@
+// const Router = require("express");
+// const fs = require("fs");
+
+// const router = Router();
+
+// router.get("/", (res, req) => {
+// 	const todos = JSON.parse(fs.readFileSync("../todos.json", "utf8"));
+
+// 	if (todos == "undefined") {
+// 	} else {
+// 	}
+// });
+
+// exports.router = router;
--- server/server.js
+++ server/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+// const router = require("./router");
 const fs = require("fs");
 
 const port = 8000;
@@ -7,6 +8,22 @@ const app = express();
 
 app.use(express.json());
 
+app.get("/todos", (req, res) => {
+	const todos = fs.readFileSync("../todos.json", "utf-8");
+
+	if (todos.length == 0) {
+		res.json({ message: "You dont have any todos yet!" });
+	} else {
+		const parsedTodos = JSON.parse(todos);
+		res.json(parsedTodos);
+	}
+});
+
+app.post("/todos", () => {});
+
+const todos = fs.readFileSync("../todos.json", "utf-8");
+
+console.log(typeof todos);
 app.listen(port, () => {
 	console.log(`Server is listening on port: ${port}`);
 });
