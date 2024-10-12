37e43e1c7daf00027d60f62e9366bff7a8892b8b
zsolt270 <zsolt270000@gmail.com>
2024-10-12 14:29:28 +0200

    added switch case

--- client/client.js
+++ client/client.js
@@ -1,34 +1,37 @@
-const { response } = require('express');
+const prompt = require("prompt-sync")();
 
-const prompt = require('prompt-sync')();
+const command = prompt("Please give me a command, u would like to do!");
 
-const hi = prompt("Hi! Let's see your todos! Press Enter!")
-console.log(hi);
-console.log(typeof hi);
-// fetch('http://localhost:8000/todos')
-//   .then(response => response.json())
-//   .then(data => {
-//     console.log(data)
-//   })
-if ( hi.length != 0) {
-  fetch(`http://localhost:8000/todos/${hi}`, {
-    method: "DELETE",
-    headers: {"Content-Type" : "application/json"}
-  })
-   .then(response => response.json())
-   .then(data => {
-    console.log(data);
-   })
-} else {
-  console.log("error");
+switch (command) {
+	case "read-todos":
+		fetch("http://localhost:8000/todos")
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
+		break;
+	case "create":
+		break;
+	case "update":
+		break;
+	case "delete":
+		id = prompt("Please give the ID of the todo u would like to delete!");
+		if (id.length != 0) {
+			fetch(`http://localhost:8000/todos/${id}`, {
+				method: "DELETE",
+				headers: { "Content-Type": "application/json" },
+			})
+				.then((response) => response.json())
+				.then((data) => {
+					console.log(data);
+				});
+		} else {
+			console.log("error");
+		}
+		break;
+	default:
+		throw new Error(`There isnt a ${command} type of command!`);
 }
 
-
-
-  // switch (command) {
-  //   case "get":
-  //     break;
-  //   case "put":
-  //     break;
-  //   default:
-  // }
+console.log(hi);
+console.log(typeof hi);
