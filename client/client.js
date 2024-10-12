1a6782d1a8b993c0073776d2e4c88fced544f6fa
zsolt270 <zsolt270000@gmail.com>
2024-10-12 19:04:25 +0200

    made the client side of the feature

--- client/client.js
+++ client/client.js
@@ -3,12 +3,30 @@ const prompt = require("prompt-sync")();
 const command = prompt("Give me a command, u would like to do! ");
 
 switch (command) {
-	case "read-todos":
+	case "read-all-todos":
 		fetch("http://localhost:8000/todos")
 			.then((response) => response.json())
 			.then((data) => {
 				console.log(data);
 			});
+		break;
+	case "read-filtered-todos":
+		const filter = prompt(
+			"Which todos do u want to get (completed or not-completed)? "
+		);
+		const re = /(^not.completed|notcompleted|completed)/i;
+
+		if (!re.test(filter)) {
+			console.log(filter);
+			throw new Error("The filter must be either completed or not-completed");
+		}
+
+		fetch(`http://localhost:8000/todos/${filter}`)
+			.then((response) => response.json())
+			.then((data) => {
+				console.log(data);
+			});
+
 		break;
 	case "create":
 		const givenTitle = prompt("Give a Todo title! ");
@@ -27,7 +45,9 @@ switch (command) {
 			.then((response) => response.json())
 			.then((data) => console.log(data));
 		break;
-	case "update":
+	case "update-todo-title":
+		break;
+	case "update-todo-status":
 		break;
 	case "delete":
 		id = prompt("Give the ID of the todo u would like to delete!");
